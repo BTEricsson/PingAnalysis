a15531579b635fadaf830bc3a481882fadbbecaa
Thomas Ericsson <thomas.ericsson@getitright.se>
2023-07-31 20:04:54 +0200

    fix(Extention, Service): added TextColor in LogView

---
+++ Analysis/Extentions/StringColorExtentions.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+namespace Analysis.Extentions
+{
+    public static class StringColorExtentions
+    {
+        public static Color GetStatusColor(this string Status)
+        {
+
+            switch (Status)
+            {
+                case "not installed":
+                    return Color.OrangeRed;
+                case "Running":
+                    return Color.Green;
+                case "Stopped":
+                    return Color.Red;
+                default:
+                    return Color.Black;
+            }
+        }
+
+        public static Color GetLineColor(this string TextLine)
+        {
+            if (TextLine.Contains("Success"))
+                return Color.Green;
+
+            if (TextLine.Contains("UP"))
+                return Color.Green;
+
+            if (TextLine.Contains("Down"))
+                return Color.Red;
+
+            if (TextLine.Contains("DestinationHostUnreachable"))
+                return Color.Red;
+
+            if (TextLine.Contains("Host not responding"))
+                return Color.Red;
+
+            return Color.Black;
+        }
+    }
+}
